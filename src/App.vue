238cc69ab0e599b9992e42894c62f4e21df28437
Lythong <thongking1280@gmail.com>
2025-10-31 00:19:29 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 
 <template>
   <h1>Hello This is my first vue project</h1>
-  <!-- <h3>អស្ចារ្យណាស់​ កូនខ្មែរ</h3> -->
-  <!-- <p>កូនខ្មែរមួយរូបទៀតហើយ​ យប់មិនដេក ថ្ងៃឡើងលេង​តែហ្កេម</p> -->
 </template>
 
 <style scoped></style>
